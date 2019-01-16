072dc15a5ebe395314b8c580bc3c8d3d95153615
Vittorio Retrivi <retrovitto@gmail.com>
2019-01-15 23:31:24 -0300

    añadido "Phyllotaxis"

---
+++ Phyllotaxis/js/script.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas_container = document.getElementById("canvas_container");
+
+var offSetLeft = canvas.offsetLeft;
+var offSetTop = canvas.offsetTop;
+var width = canvas.width = canvas_container.clientWidth;
+var height = canvas.height = canvas_container.clientHeight;
+
+window.addEventListener('resize', function() {
+  offSetLeft = canvas.offsetLeft;
+  offSetTop = canvas.offsetTop;
+  width = canvas.width = canvas_container.clientWidth;
+  height = canvas.height = canvas_container.clientHeight;
+
+  generate(137.5,6);
+
+});
+
+// --------------------------------------------------------------------
+
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+function generate(angle,cValue){
+
+  ctx.translate(width/2,height/2);
+
+  let a = Math.radians(angle);
+  let r = 0;
+  let c = cValue;
+
+  for (var n = 0; n < 4000; n++) {
+
+    let x = r * Math.cos(a*n);
+    let y = r * Math.sin(a*n);
+
+    ctx.beginPath();
+    ctx.fillStyle = `rgb(${(a*r) % 255},${(a*n) % 255},${(r*n) % 255})`;
+    ctx.arc(x, y, 3, 0, 2 * Math.PI);
+    ctx.fill();
+
+    r = c * Math.sqrt(n);
+
+  }
+
+}
+
+generate(137.5,6);
