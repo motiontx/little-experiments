11618ac98957ceeb43ecb7f63fa57ea83e383bfd
Vittorio Retrivi <retrovitto@gmail.com>
2019-02-10 00:07:06 -0300

    Finalizado Piano

--- experiments/Piano/js/script.js
+++ experiments/Piano/js/script.js
@@ -1,9 +1,6 @@
-// --------------------------------------------------------------------
-
-let ctx = new AudioContext();
+const ctx = new AudioContext();
 
 function playSound(frequency) {
-
   let wave = document.querySelector('input[name="wave"]:checked').value;
 
   let osc = ctx.createOscillator();
@@ -13,8 +10,57 @@ function playSound(frequency) {
   osc.frequency.value = frequency;
   osc.connect(gain);
 
-  gain.gain.exponentialRampToValueAtTime(0.00001, ctx.currentTime + 1.5);
+  let releaseValue = ctx.currentTime + 2;
+  gain.gain.exponentialRampToValueAtTime(0.00001, releaseValue);
   gain.connect(ctx.destination);
 
   osc.start(0);
+  osc.stop(releaseValue);
+}
+
+/*-----------------------------------------------------------*/
+
+let key_names = document.getElementsByClassName('key-name');
+let key_numbers = document.getElementsByClassName('key-number');
+
+function showHelp() {
+  let help = document.getElementById('help').checked;
+  let display = help ? "block" : "none";
+  for (let el of key_names) {
+    el.style.display = display;
+  }
+  for (let el of key_numbers) {
+    el.style.display = display;
+  }
+}
+
+/*-----------------------------------------------------------*/
+
+let playing = false;
+
+let step = 0;
+let longSong = song.length;
+
+function playSample() {
+  if (playing) {
+    if (song[step][0] != "Blank") {
+      playSound(notesName[song[step][0]]);
+    }
+
+    setTimeout(playSample, song[step][1] * 0.8);
+
+    step++;
+    if (step == longSong) {
+      step = 0;
+    }
+  }
+}
+
+function startStopSample() {
+  if (playing) {
+    playing = false;
+  } else {
+    playing = true;
+    playSample();
+  }
 }
