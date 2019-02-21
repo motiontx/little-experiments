f99c4f5c49f5ae23f9ed05bcb85aa507f5efbe62
Vittorio Retrivi <retrovitto@gmail.com>
2019-02-21 00:18:48 -0300

    Feature en Piano

--- experiments/Piano/js/script.js
+++ experiments/Piano/js/script.js
@@ -1,7 +1,20 @@
+const keys = {};
+
+for (let key of Object.keys(notes)) {
+  keys[key] = document.getElementById(key);
+}
+
+function pressKey(key) {
+  keys[key].classList.add("pressed");
+  setTimeout(() => {keys[key].classList.remove("pressed")}, 150);
+}
+
+/*-----------------------------------------------------------*/
+
 const ctx = new AudioContext();
 
-function playSound(frequency) {
-  let wave = document.querySelector('input[name="wave"]:checked').value;
+function playSound(frequency, waveType) {
+  let wave = waveType || document.querySelector('input[name="wave"]:checked').value;
 
   let osc = ctx.createOscillator();
   let gain = ctx.createGain();
@@ -11,6 +24,7 @@ function playSound(frequency) {
   osc.connect(gain);
 
   let releaseValue = ctx.currentTime + 2;
+  gain.gain.value = 0.3;
   gain.gain.exponentialRampToValueAtTime(0.00001, releaseValue);
   gain.connect(ctx.destination);
 
@@ -43,7 +57,8 @@ let step = 0;
 function playSample() {
   if (playing) {
     if (song[step][0] != "Blank") {
-      playSound(notesName[song[step][0]]);
+      playSound(notes[song[step][0]]);
+      pressKey(song[step][0]);
     }
     setTimeout(playSample, song[step][1] * 0.8);
     step++;
