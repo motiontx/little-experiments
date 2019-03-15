0f26a27c23de0bd7fcb60bbb9d10413d67253e3d
Vittorio Retrivi <retrovitto@gmail.com>
2019-03-14 22:31:01 -0300

    Añadido próximo proyecto

---
+++ experiments/Ray-Casting/js/script.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas_container = document.getElementById("canvas_container");
+
+let offSetLeft = canvas.offsetLeft;
+let offSetTop = canvas.offsetTop;
+let width = canvas.width = canvas_container.clientWidth;
+let height = canvas.height = canvas_container.clientHeight;
+
+window.addEventListener('resize', function() {
+  offSetLeft = canvas.offsetLeft;
+  offSetTop = canvas.offsetTop;
+  width = canvas.width = canvas_container.clientWidth;
+  height = canvas.height = canvas_container.clientHeight;
+});
+
+// --------------------------------------------------------------------
