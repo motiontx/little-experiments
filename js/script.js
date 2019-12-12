c1f3017f57e7218b863118f2fa41d76f911713a9
Vittorio Retrivi <retrovitto@gmail.com>
2019-12-11 22:50:59 -0300

    Added Iframe

--- js/script.js
+++ js/script.js
@@ -57,3 +57,22 @@ function draw(e, touch){
 
   ctx.stroke();
 }
+
+
+// --------------------------------------------------------------------
+
+const home = document.getElementById('home');
+const display = document.getElementById('display');
+const frame = document.getElementById('frame');
+
+const backToHome = () => {
+  home.style.display = "flex";
+  display.style.display = "none";
+  frame.src = "";
+}
+
+const setExperiment = (name) => {
+  display.style.display = "block";
+  home.style.display = "none";
+  frame.src = `./experiments/${name}/index.html`;
+}
