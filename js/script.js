262ff42e3c247fe5d2c338e7595890a1c5717f23
Vittorio Retrivi <retrovitto@gmail.com>
2020-01-09 17:01:59 -0300

    Added Categories

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ const setExperiment = (name) => {
   home.style.display = "none";
   frame.src = `./experiments/${name}/index.html`;
 }
+
+// ----------------------------------------------------------------- //
+
+const starInput = document.getElementById('star-input');
+const wipInput = document.getElementById('wip-input');
+const othersInput = document.getElementById('others-input');
+
+const stars = document.getElementsByClassName('star-link');
+const wips = document.getElementsByClassName('wip-link');
+const others = document.getElementsByClassName('others-link');
+
+const updateCategories = () => {
+  for (let star of stars) star.classList.add('d-none');
+  for (let wip of wips) wip.classList.add('d-none');
+  for (let other of others) other.classList.add('d-none');
+
+  if (starInput.checked) for (let star of stars) star.classList.remove('d-none');
+  if (wipInput.checked) for (let wip of wips) wip.classList.remove('d-none');
+  if (othersInput.checked) for (let other of others) other.classList.remove('d-none');
+}
