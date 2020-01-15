d880ab12650539551f3547a9a51b56d0e66b3733
Vittorio Retrivi <retrovitto@gmail.com>
2020-01-15 16:45:06 -0300

    Update Sliding Puzzle

--- experiments/Sliding-Puzzle/js/script.js
+++ experiments/Sliding-Puzzle/js/script.js
@@ -8,12 +8,55 @@ function create() {
 
 class Game {
   constructor() {
-    this.matrix = [];
     this.size = 3;
     this.movements = 0;
+    this.playing = false;
+
+    this.matrix = [];
 
     this.board = document.getElementById('pieces');
+    this.boardMovements = document.getElementById('movements');
+    this.boardTime = document.getElementById('timer');
 
+    this.reset();
+
+  }
+
+  reset(){
+    this.playing = false;
+
+    this.resetTimer();
+    this.resetMovements()
+
+    this.resetMatrix();
+    this.resetBoard();
+  }
+
+  startTimer() {
+    this.startTime = new Date()
+    this.timer = setInterval(() => {
+      this.boardTime.innerHTML = ((new Date() - game.startTime) / 1000).toFixed(2)
+    }, 100)
+  }
+
+  resetTimer() {
+    clearInterval(this.timer);
+    this.timer = false;
+    this.boardTime.innerHTML = '0.00'
+  }
+
+  pauseTimer() {
+    clearInterval(this.timer);
+    this.timer = false;
+  }
+
+  resetMovements(){
+    this.movements = 0;
+    this.updateBoardMovements();
+  }
+
+  updateBoardMovements(){
+    this.boardMovements.innerHTML = this.movements;
   }
 
   resetMatrix(){
@@ -38,6 +81,7 @@ class Game {
       for (let j = 0; j < this.matrix[i].length; j++) {
         let number = this.matrix[i][j];
         if (number != -1) {
+
           let div = document.createElement('div');
           div.innerHTML = number;
           div.className = "piece";
@@ -57,24 +101,36 @@ class Game {
       }
     }
 
+    this.updateBoardMovements();
   }
 
   movePiece(piece){
-
     let x = parseInt(piece.getAttribute('x'));
     let y = parseInt(piece.getAttribute('y'));
     let pieceLength = 100/this.size;
     let newPos = this.NeighborIsEmpty(x,y);
     if (newPos) {
-      let number = this.matrix[y][x];
-      this.matrix[newPos.y][newPos.x] = number;
+
+      if (!this.playing) {
+        this.startPlaying();
+      }
+
+      let n = this.matrix[y][x];
+      this.matrix[newPos.y][newPos.x] = n;
       this.matrix[y][x] = -1;
       piece.setAttribute('x', newPos.x);
       piece.setAttribute('y', newPos.y);
       piece.style.top = `${newPos.y*pieceLength}%`;
       piece.style.left = `${newPos.x*pieceLength}%`;
-    }
 
+      this.movements++;
+      this.updateBoardMovements();
+
+      if (this.checkIfIWin()) {
+        this.gameWon();
+      }
+
+    }
   }
 
   NeighborIsEmpty(x,y){
@@ -85,12 +141,35 @@ class Game {
     return false;
   }
 
-  reset(){
-    this.resetMatrix();
-    this.resetBoard();
+  startPlaying(){
+    this.resetTimer();
+    this.resetMovements();
+    this.startTimer();
+    this.playing = true;
+  }
+
+  checkIfIWin(){
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (i == this.size-1 && j == this.size-1) {
+          if (this.matrix[i][j] != -1) {
+            return false;
+          }
+        }
+        else if (this.matrix[i][j] != j+(i*this.size)+1) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  gameWon() {
+    this.pauseTimer();
+    this.playing = false;
+    console.log("ganaste");
   }
 
 }
 
 let game = new Game();
-game.reset();
