76be25b9f717a2f7c53b83f994393d29538b5433
Vittorio Retrivi <retrovitto@gmail.com>
2020-02-22 21:17:52 -0300

    Update Chess JS

--- experiments/Chess-JS/js/game.js
+++ experiments/Chess-JS/js/game.js
@@ -4,8 +4,90 @@
 
 class Game {
   constructor() {
+    this.pieces = [];
+    this.resetBoard();
+
 
   }
+
+  // ----------------------------------------------------------------- //
+
+  resetBoard(){
+    this.boardMatrix = [];
+    this.board = document.getElementById('board');
+    this.board.innerHTML = "";
+    for (let i = 0; i < 8; i++) {
+      let row = [];
+      for (let j = 0; j < 8; j++) {
+        let cell = document.createElement("cell");
+
+        cell.addEventListener('click', (el) => {
+          let x = el.target.getAttribute('x');
+          let y = el.target.getAttribute('y');
+          this.actionAt(x,y);
+        });
+
+        cell.setAttribute("x", j);
+        cell.setAttribute("y", i);
+        this.board.appendChild(cell);
+        row.push(cell);
+      }
+      this.boardMatrix.push(row);
+    }
+  }
+
+  // ----------------------------------------------------------------- //
+
+  updateBoard(){
+    for (let row of this.boardMatrix) {
+      for (let cell of row) {
+        cell.className = "";
+      }
+    }
+
+    for (let piece of this.pieces) {
+      let x = piece.x;
+      let y = piece.y;
+      let cell = this.boardMatrix[y][x];
+      cell.className = `${piece.type} ${piece.color}`
+    }
+  }
+
+  setEmpty() {
+    this.pieces = [];
+    this.updateBoard();
+  };
+
+  setStandardGame(){
+    this.pieces = [];
+    for (let j = 0; j < 8; j++) {
+      this.pieces.push(new Pawn(j,1, "black"));
+      this.pieces.push(new Pawn(j,6, "white"));
+    }
+
+    this.pieces.push(new Rook(0,0, "black"));
+    this.pieces.push(new Knight(1,0, "black"));
+    this.pieces.push(new Bishop(2,0, "black"));
+    this.pieces.push(new Queen(3,0, "black"));
+    this.pieces.push(new King(4,0, "black"));
+    this.pieces.push(new Bishop(5,0, "black"));
+    this.pieces.push(new Knight(6,0, "black"));
+    this.pieces.push(new Rook(7,0, "black"));
+
+    this.pieces.push(new Rook(0,7, "white"));
+    this.pieces.push(new Knight(1,7, "white"));
+    this.pieces.push(new Bishop(2,7, "white"));
+    this.pieces.push(new Queen(3,7, "white"));
+    this.pieces.push(new King(4,7, "white"));
+    this.pieces.push(new Bishop(5,7, "white"));
+    this.pieces.push(new Knight(6,7, "white"));
+    this.pieces.push(new Rook(7,7, "white"));
+
+    this.updateBoard();
+  }
+
+  set
+
 }
 
 // ----------------------------------------------------------------- //
---
+++ experiments/Chess-JS/js/piece.js
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------- //
+// ------------- ►►► </> with ♥ by Vittorio Retrivi ◄◄◄ ------------ //
+// ----------------------------------------------------------------- //
+
+class Piece {
+  constructor(x, y, color) {
+    this.type = null;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.moved = false;
+  }
+}
